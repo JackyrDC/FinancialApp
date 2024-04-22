a79d646517395e8e1151901230fcedd3670621c5
zzMAZzz <mzelayaf@unah.hn>
2024-04-21 19:15:41 -0600

    Update Header.jsx

--- FinancialApp/src/components/Header.jsx
+++ FinancialApp/src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 // Icons
 import { RiSearch2Line } from "react-icons/ri";
 
 const Header = () => {
+  const { user, isAuthenticated, isLoading, logout } = useAuth0();
   return (
     <header className="flex flex-col md:flex-row items-center justify-between gap-4">
       <h1 className="text-2xl md:text-3xl font-bold">
-        🌞 Buenos dias, <span className="text-primary-100">Miguel</span>
+        🌞 Buenos dias, <span className="text-primary-100">{user.name}</span>
       </h1>
     </header>
   );
