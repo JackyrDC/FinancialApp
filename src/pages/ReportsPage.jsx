d748e1628b51ab592ae613397501171429b4f4ce
JackyrDC <djackyrcalix@gmail.com>
2024-05-01 23:14:40 -0600

    feat: 🎸 Primer prueba de reportes

--- src/pages/ReportsPage.jsx
+++ src/pages/ReportsPage.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import PocketBaseContext from 'PocketBaseContext' 
+import { useContext } from 'react'
 
-export const ReportsPage = () => {
+
+export const ReportsPage = async() => {
+  const pb = useContext(PocketBaseContext)
+  const user = pb.authStore.model.id
+  const userExpenses = await pb.collection("expenses").getFullList(1,50,{filter:'user = '+ user })
+  console.log(userExpenses)
+  
   return (
-    <h1>ReportsPage</h1>
+    <h1>
+
+    </h1>
   )
 }
