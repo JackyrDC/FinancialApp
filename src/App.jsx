a2731dd7d62d26ad7496493444af9eca78fe8666
zzMAZzz <mzelayaf@unah.hn>
2024-05-01 19:42:37 -0600

    routers and page switching

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Login from './pages/Login';
+import Register from './pages/Register';
+import { HomePage,
+  DashboardPage,
+  HistoryPage,
+  ReportsPage
+} from './pages';
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import { HomePage } from './pages/HomePage';
-import Register from './pages/Register';
+
 import PocketBaseProvider from './pages/PocketBaseProvider';
 import { PrivateRoute } from './pages/PrivateRoute';
 
@@ -12,16 +17,30 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <PrivateRoute element={<HomePage />} />,
+    children: [
+      {
+          path: '/',
+          element: <DashboardPage />,
+      },
+      {
+          path: '/history',
+          element: <HistoryPage />,
+      },
+      {
+          path: '/reports',
+          element: <ReportsPage />,
+      },]
   },
   {
-    path: "/login",
-    element: <Login />,
+      path: '/login',
+      element: <Login />,
   },
   {
     path: "/register",
     element: <Register />,
   }
 ]);
+
 
 function App() {
   return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
-export * from './LoginPage';
-export * from './RegisterPage';
-export * from './AuthPage';
-export * from './Logout';
-export * from './Profile';
-export * from './HomePage';
+export * from './AuthPage'; // Eliminar luego
 export * from './DashboardPage';
-export * from './TransactionsPage';
-export * from './ReportsPage';
-
+export * from './HistoryPage';
+export * from './HomePage';
 export * from './Login';
+export * from './LoginPage'; // Eliminar luego
+export * from './Logout'; // Eliminar luego
+export * from './Profile'; // Eliminar luego
+export * from './Register';
+export * from './RegisterPage'; // Eliminar luego
+export * from './ReportsPage';
+export * from './TransactionsPage';
